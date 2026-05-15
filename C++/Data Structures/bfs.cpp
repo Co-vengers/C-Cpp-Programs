971989c1e251435509740115184b7be4dce81ac9
Co-vengers <vyomrohila@gmail.com>
2026-05-15 12:12:45 +0530

    Code for BFS Traversal in un-directed graph

---
+++ C++/Data Structures/bfs.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class BFS{
+public:
+	vector<vector<int>> createAdjList(int vertices, vector<pair<int, int>> edges) {
+    
+    	// Initialize matrix with 0
+	    vector<vector<int>> adjList(vertices, vector<int>(vertices, 0));
+
+	    // Fill edges
+	    for(auto edge : edges) {
+	        int u = edge.first;
+	        int v = edge.second;
+
+	        adjList[u].push_back(v);
+	        adjList[v].push_back(u);   // Remove this line for directed graph
+	    }
+
+	    return adjList;
+	}
+
+	vector<int> bfsOfGraph(int v, vector<vector<int>> &adjList){
+
+		vector<int> visited(v);
+		queue<int> q;
+
+		visited[0] = 1;
+		q.push(0);
+
+		vector<int> bfs;
+
+		while(!q.empty()){
+			int parent = q.front();
+			q.pop();
+			for(auto &child : adjList[parent]){
+				if(!visited[child]){
+					visited[child] = 1;
+					q.push(child);
+				}
+			}
+			bfs.push_back(parent);
+		}
+
+		return bfs;
+	}
+};
+
+int main() {
+
+	BFS bfs;
+
+    int vertices = 4;
+
+    vector<pair<int, int>> edges = {
+        {0,1},
+        {0,2},
+        {1,2},
+        {2,3}
+    };
+
+    vector<vector<int>> list = bfs.createAdjList(vertices, edges);
+
+    vector<int> res = bfs.bfsOfGraph(vertices, list);
+
+    cout<<"BFS Traversal:";
+    for(auto &val : res){
+    	cout<<val<<" ";
+    }
+
+    return 0;
+}
