d7fdf3c9dccb7c394b13390e8bc43429d5bc7c78
Co-vengers <vyomrohila@gmail.com>
2026-08-09 00:01:43 +0530

    ____*    ***   *****  ******* *********

---
+++ C++/Concepts/Patterns/pattern5.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    void pattern1(int n){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = n-1 ; j > i ; j--){
+                cout<<" ";
+            }
+            for(int k = 0 ; k < 2*i+1 ; k++){
+                cout<<"*";
+            }
+            cout<<"\n";
+        }
+    }
+};
+
+int main(){
+    Solution s1;
+    int n = 5;
+    // cin>>n;
+    s1.pattern1(n);
+    return 0;
+}
+
+// output:
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
