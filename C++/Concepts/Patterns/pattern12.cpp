7d1988939ad6b45ab1873fce65090b9b87ab31d9
Co-vengers <vyomrohila@gmail.com>
2026-08-09 23:45:14 +0530

    A AB ABC ABCD ABCDE

---
+++ C++/Concepts/Patterns/pattern12.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    void pattern1(int n){
+        for(int i = 0 ; i < n ; i++){
+            for(char ch = 'A' ; ch <= 'A' + i ; ch++){
+                cout<<ch;
+            }
+            cout<<"\n";
+        }
+    }
+};
+
+int main(){
+    Solution s1;
+    int n = 5;
+    // cin>>n;
+    s1.pattern1(n);
+    return 0;
+}
+
+// output:
+
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
